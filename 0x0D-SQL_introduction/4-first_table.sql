095c35a19e2f16a32ef53b52a87fe7837ac65cd5
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-12-12 21:09:20 +0100

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
--- write a script that createscreate a table called first_table in the current database
-CREATE table IF NOT EXISTS first_table("id" INT, "name" VARCHAR(256));
+-- create a table called "first_table"
+-- "first_table" should have (id INT) and (name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
