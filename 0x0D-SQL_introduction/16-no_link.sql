0a436e019e7797e33af9ae43c1ae45e4cc27ef48
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-12-12 23:18:32 +0100

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- Computes the average score of all records in the table second_table in my MySQL server.
-SELECT `name`, `score` FROM `second_table`
+SELECT `score`, `name` FROM `second_table`
 ORDER BY `score`
 DESC;
