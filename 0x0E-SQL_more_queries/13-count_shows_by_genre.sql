74eb1481227ebfadd053a43474f45fbe40ba3d77
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-12-13 18:49:51 +0100

    SQL-More-Queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
---lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- lists all rows of a database meeting a condition
+-- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
