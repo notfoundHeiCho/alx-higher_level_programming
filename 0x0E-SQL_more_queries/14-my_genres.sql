94310b8a39f1ff6f864ab828efd88cd0623ec733
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-12-13 18:53:00 +0100

    SQL-More-Queries

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- uses the hbtn_0d_tvshows db to lists all genres of the show Dexter
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+       FROM tv_show_genres
+       INNER JOIN tv_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY tv_genres.name
+       ORDER BY number_shows DESC;
+       
