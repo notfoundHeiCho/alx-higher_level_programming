18c8e0f0467456ed711fdf7cb90fbab9d15b184d
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-12-13 19:54:47 +0100

    SQL-More-Queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- lists all genres in the db hbtn_0d_tvshows_rate by their rating
 -- list all rows in a db linked to a row in another table
 SELECT name, SUM(tv_show_rating.rate) 'rating'
-    FROM tv_genres
+FROM tv_genres
 	JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 	JOIN tv_shows
