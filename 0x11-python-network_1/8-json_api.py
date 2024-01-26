b815d1a3fd0991dc162c01aef4fe341d06a68f0a
ZZZHajar008 <ilyass.infinite345@gmail.com>
2024-01-26 20:30:56 +0100

    Python - network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
+"""Sends a search parameter to a URL."""
 
-import sys
-import requests
+if __name__ == '__main__':
+    from requests import post
+    from sys import argv
 
-if __name__ == "__main__":
-    if len(sys-argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
-    url = "http://0.0.0.0:5000/search_user"
-    params = {"q": q}
-    res = requests.post(url, data=params)
-    try:
-        resjson = res.json()
-        if resjson:
-            print(f'[{resjson["id"]}] {resjson["name"]}')
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    response = post(URL, data)
+
+    type_res = response.headers['content-type']
+
+    if type_res == 'application/json':
+        result = response.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            print("[{}] {}".format(_id, name))
         else:
-            print("No result")
-    except Exception:
-        print("Not a valid JSON")
+            print('No result')
+    else:
+        print('Not a valid JSON')
