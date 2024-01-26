0021148be456af21043ef05311ac10c1c197c8df
ZZZHajar008 <ilyass.infinite345@gmail.com>
2024-01-26 20:12:43 +0100

    Fix post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""Sends a form data to a URL."""
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
+
 import urllib.request
 import urllib.parse
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    params = {"email": email}
-    utf_params = urllib.parse.urlencode(params).encode('utf-8')
+if __name__ == '__main__':
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
+
+    with urllib.request.urlopen(url, DATA) as response:
+        print(response.read().decode('utf-8'))
