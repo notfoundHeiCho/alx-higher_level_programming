e6bfa0a413bea879f26c657fc23462702e0a3598
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-02-20 18:33:39 +0100

    Task 0 Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
-const sf = require('fs');
-sf.fstat.readfile(process.argv[2], 'utf-8', function (error, content) {
-  console.log(error || content);
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
