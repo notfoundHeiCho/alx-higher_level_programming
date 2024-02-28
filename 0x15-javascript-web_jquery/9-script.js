3b4fc38d1a796bf36ccd4c784fef3bb6c777ea94
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-02-28 23:48:47 +0100

    9. Say Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* global $ */
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    success: function (data) {
+      $('DIV#hello').text(data.hello);
+    }
+  });
+});
