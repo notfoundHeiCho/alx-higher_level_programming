1745b20059cca1daf748e50a618d259dc6f542e9
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-02-28 23:45:52 +0100

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* global $ */
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  success: data => {
+    $.each(data.results, (i, film) => {
+      $('UL#list_movies').append('<li>' + film.title + '</li>');
+    });
+  }
+});
